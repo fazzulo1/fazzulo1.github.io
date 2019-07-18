69d71271f5774760fef35a98b37f61ee82266471
haroldjohan <haroldmiranda1@Harolds-MacBook-Air.local>
2019-07-18 08:58:13 -0400

    Project morning update: modified initial interface design, added backgrounds, made it responsive

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 $(() => {
   const getProvider = data => {
-    for (let i = 0; i < $('.text0').val(); i++) {
-      console.log(data.network.stations[i]);
-      console.log(data.network.stations[i].name);
-      const $list = $('<h3>').text(data.network.stations[i].name);
-      const $details = $('<h3>').text(
-        `This station has:${
+    console.log(data.network.stations.length);
+    for (let i = 0; i < data.network.stations.length; i++) {
+      const $list = $('<div>').text(
+        `Station:${data.network.stations[i].name}, free bikes:${
           data.network.stations[i].free_bikes
-        } bikes to rent and ${
-          data.network.stations[i].empty_slots
-        } spots to dock your bike`
+        }, free docks:${data.network.stations[i].empty_slots}`
       );
-      console.log(
-        `This stations has:${data.network.stations[i].free_bikes} and ${
-          data.network.stations[i].empty_slots
-        } spots`
-      );
-
+      $list.addClass('list');
       $('#providers').append($list);
-      $('#details').append($details);
     }
+    const $cityName = $('<h4>')
+      .text(`City: ${data.network.location.city}`)
+      .addClass('createdTitle');
+    const $companyName = $('<h4>')
+      .text(`Company Name: ${data.network.name}`)
+      .addClass('createdTitle');
+    const $numStations = $('<h4>')
+      .text(`Number of Stations: ${data.network.stations.length}`)
+      .addClass('createdTitle');
+    $companyName.css('background-color', 'rgb(207, 187, 7)');
+    $cityName.css('background-color', 'rgb(207, 187, 7)');
+    $numStations.css('background-color', 'rgb(207, 187, 7)');
+
+    $('.company').append($cityName);
+    $('.company').append($companyName);
+    $('.company').append($numStations);
   };
 
   ///////////////////////////////////////////////////////
   $('.button').on('click', event => {
-    event.preventDefault(); // stop page from reloading
-
-    const endpoint = `https://api.citybik.es/v2/networks/citi-bike-nyc`;
+    event.preventDefault();
+    // const endpoint = `https://api.citybik.es/v2/networks?`;
+    const endpoint = `https://api.citybik.es/v2/networks/bicing`;
     $.ajax({ url: endpoint }).then(getProvider); // get data asynchronously, when the data gets back, handle it
   });
+  // $('.button').on('click', event => {
+  //   event.preventDefault();
+  //   // const endpoint = `https://api.citybik.es/v2/networks?`;
+  //   const endpoint = `https://api.citybik.es/v2/networks/citi-bike-nyc`;
+  //   $.ajax({ url: endpoint }).then(getProvider); // get data asynchronously, when the data gets back, handle it
+  // });
 });
