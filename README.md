498c0ee798dffa0ae64b3dc1db4f82b6322fe8d9
haroldjohan <haroldmiranda1@Harolds-Air.home>
2019-07-21 19:29:45 -0400

    made an instructions update and added a function to locate a specific bike station

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # fazzulo1.github.io
-# Web App Name: findaBIKE. 
+
+# Web App Name: findaBIKE.
+
 # Goal: This web app helps to find countries and cities where a bicycle rental is available.
+
 # Languages and Libraries: HTML, CSS, JS, Jquery.
 
 ![Screen Shot 2019-07-19 at 3 34 17 PM](https://user-images.githubusercontent.com/51187016/61560656-e21eb880-aa3a-11e9-88c2-48c7c3281145.png)
 
 # Functionality:
 
-  # 1. Popup alert will notify the user to CLICK on a GREEN button to look for a bike rental company on available countries and cities.
-  # 1.1 The first panel will be populated with a data set showing CITY and COUNTRY, as well as the API link corresponding to the specific company providing the bike rental service.
-  
-  # 2. A second popup alert will provide instructions on how to search for a specific country or city. 
-   # 2.1 The search can be performed by SCROLLING DOWN the populated area or by pressing CTRL + F to search for a specific city or country.
-  # 2.2 Recommended option: Search by city, just look for the name or type the name. For example, CTRL +F, then type "Paris"
-  # 2.3 Less recommended option: Search by country, the user should search by using a country code. For example, United States (US), Germany (DE), Italy (IT), Spain (ES), Mexico (MX), etc.
-  # 2.4 After clicking on the intended country or city. A second panel will be populated, it will include the station address, number of free bikes for rental and number of docking stations. 
-  
-  # 3. A Google map will display the location of the first populated bike station.
- 
+# 1. Popup alert will notify the user to CLICK on a GREEN button to look for a bike rental company on available countries and cities.
 
+# 2. The first panel, in blue, will be populated with a data set showing CITY and COUNTRY, as well as the API link corresponding to the specific company providing the bike rental service.
 
+# 2.1 A note in gold will provide the following instruction "Click on any squares in the blue panel to get a bike station in the city. (Scroll down or press CTL+F to look for it)"
+
+# 2.2 The search can be performed by SCROLLING DOWN the populated area or by pressing COMMAND+F to search for a specific city or country.
+
+# 2.3 Recommended option: Search by city, just look for the name or type the name. For example, COMMAND+F, then type "Paris"
+
+# 2.4 Less recommended option: Search by country, the user should search by using a country code. For example, United States (US), Germany (DE), Italy (IT), Spain (ES), Mexico (MX), etc.
+
+# 2.5 A Google map will display the city / country location.
+
+# 3. After clicking on the intended city / country. A second panel, in green, will be populated, it will include the station address, number of free bikes for rental, number of docking stations, and latitude / longitude information.
+
+# 3.1 A note in gold will provide the following instruction "Click on any square in the green panel to get an exact bike station address in the map (Scroll down or press COMMAND+F to look for it)"
+
+# 3. A Google map will display the exact bike location.
--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 $(() => {
   alert(
-    'To find available countries and cities where you can rent a BIKE, CLICK on the GREEN BUTTON'
+    'To find available cities/countries where you can rent a BIKE, CLICK on the GREEN BUTTON, and READ the INSTRUCTIONS in GOLD'
   );
   const getNetwork = data => {
+    const $instruction1 = $('<div>')
+      .text(
+        'Click on any square in the blue panel to get a bike station in the available cities / countries. (Scroll down or press COMMAND+F to look for it)'
+      )
+      .addClass('instruction');
+    $('#providers').append($instruction1);
     for (let i = 0; i < data.networks.length; i++) {
       const $list1 = $('<div>').text(
         `City:${data.networks[i].location.city},Country:${
@@ -15,13 +21,21 @@ $(() => {
   };
 
   const getProvider = data => {
+    const $instruction1 = $('<div>')
+      .text(
+        'Click on any square in the green panel to get an exact bike station address in the map (Scroll down or press COMMAND+F to look for it)'
+      )
+      .addClass('instruction1');
+    $('#providers2').append($instruction1);
     for (let i = 0; i < data.network.stations.length; i++) {
       const $list = $('<div>').text(
         `Station:${data.network.stations[i].name}, free bikes:${
           data.network.stations[i].free_bikes
-        }, free docks:${data.network.stations[i].empty_slots}`
+        }, free docks:${data.network.stations[i].empty_slots}, lat&long: ${
+          data.network.stations[i].latitude
+        },${data.network.stations[i].longitude}`
       );
-      $list.addClass('list');
+      $list.addClass('list1');
       $('#providers2').append($list);
 
       $('#map').empty();
@@ -61,11 +75,25 @@ $(() => {
     $('.company').append($numStations);
   };
 
+  const newMap = latlong => {
+    $('#map').empty();
+    $('#map').html(
+      `     <iframe class='map' 
+    width="100%" 
+    height="504"     
+    frameborder="0" 
+    scrolling="no" 
+    marginheight="0" 
+    marginwidth="0" 
+    src='https://maps.google.com/maps?q=${latlong}&hl=es;z=50&amp;output=embed'>
+    </iframe>`
+    );
+  };
+
   $('.button0').on('click', event => {
     event.preventDefault();
-    alert(
-      'To get specific stations where you can findaBIKE, read the instruction and click OK. STEP 1. Scroll down or press CTL+F to look for the bike station. STEP 2. CLICK on the corresponding square'
-    );
+    // alert();
+    // 'To get specific stations where you can findaBIKE, read the instructions and click OK. STEP 1. Scroll down or press CTL+F to look for the bike station. STEP 2. CLICK on the corresponding square'
 
     const endpoint = `https://api.citybik.es/v2/networks`;
     $.ajax({ url: endpoint }).then(getNetwork); // get data asynchronously, when the data gets back, handle it.
@@ -78,5 +106,12 @@ $(() => {
 
     const endpoint = `https://api.citybik.es${value1}`;
     $.ajax({ url: endpoint }).then(getProvider); // get data asynchronously, when the data gets back, handle it
+  });
+  $('.container2').on('click', '.list1', event => {
+    event.preventDefault();
+    let value2 = $(event.currentTarget).text();
+    value2 = value2.split(' ');
+    value2 = value2[value2.length - 1];
+    newMap(value2);
   });
 });
